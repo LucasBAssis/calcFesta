894389f6864b36d96f364bcbad19a0c1a20dcf77
Lucas Assis <lucas.andrade322@gmail.com>
2019-06-27 17:21:03 -0300

    Javascript de comentario

--- js/fakedbCalc.js
+++ js/fakedbCalc.js
@@ -3,7 +3,13 @@ var dbCalcCalc = {
         {
             "id": 1,
             "produto": ["Batata Frita", "Salgadinho", "Hamburguer","Bala", "Sorvete", "Refrigerante","Suco"],
-            "qtd": [590,30,12,100,5,10,20]
+            "qtd": [590,30,12,100,5,10,20],
+            "comentarios": [
+                {
+                    "nome": "Usuario 1",
+                    "comentario": "Bla bla bla"
+                }
+            ]
         }
     ]
 }
@@ -19,10 +25,24 @@ function insertCalc(calc) {
     let novaCalc = {
         "id": novoId,
         "produto": calc.produto,
-        "qtd": calc.qtd
+        "qtd": calc.qtd,
+        "comentarios": []
     };
 
     dbCalc.data.push(novaCalc);
 
+    localStorage.setItem('dbCalc', JSON.stringify(dbCalc));
+}
+
+function insertComentario(id, coment) {
+    let index = dbCalc.data.map(obj => obj.id).indexOf(id);
+
+    let novoComentario = {
+        "nome": coment.nome,
+        "comentario": coment.comentario
+    };
+
+    dbCalc.data[index].comentarios.push(novoComentario);
+    
     localStorage.setItem('dbCalc', JSON.stringify(dbCalc));
 }
